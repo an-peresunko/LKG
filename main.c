9f6bf969dc803292715eda2034418e72b242e61b
Анастасия Пересунько <anastasiaperesunko@MacBook-Air-Anastasia.local>
2022-03-01 12:17:01 +0300

    Firt commit test

---
+++ main.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// #define x = 0;
+#define a 84589
+#define c 45989
+
+unsigned int LKG(unsigned int x){
+    unsigned int X = 0;
+    X = ((a*x) + c);
+    return X;
+}
+
+int main(int argc, const char * argv[]) {
+    srand((unsigned int)(time(NULL)));
+    unsigned int X = rand();
+    
+    
+    
+    return 0;
+}
