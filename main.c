f86f7eba0d3a641f57336000101013e08dc2fd21
Ann Peresunko <an.peresunko@icloud.com>
2022-03-20 14:06:00 +0300

    new commit

--- main.c
+++ main.c
@@ -4,8 +4,11 @@
 #include <time.h>
 
 // #define x = 0;
-#define a 84589
-#define c 45989
+//#define a 84589
+//#define c 45989
+
+#define a 106
+#define c 1283
 
 unsigned int LKG(unsigned int x){
     unsigned int X = 0;
@@ -15,14 +18,27 @@ unsigned int LKG(unsigned int x){
 
 int main(int argc, const char * argv[]) {
     srand((unsigned int)(time(NULL)));
-    unsigned int X = rand();
+    unsigned int X = LKG(rand());
+    
+    unsigned int period = 0;
+    unsigned int f_gen = X; //first generation – первое сгенерированное число, нужно, чтобы найти период
     
     // Хи-квадрат критерий
     
-    // Покер критерий
+    // Покер критери
     
     // Проверка младших битов
     
+    int i = 0;
+    unsigned int tmp = X;
+    while(i!=1){
+        X = LKG(tmp);
+        //printf("%u\n", X);
+        if(X == f_gen){i = 1;}
+        period++;
+        tmp = X;
+    }
+    printf("period – %u", period);
     
     
     return 0;
